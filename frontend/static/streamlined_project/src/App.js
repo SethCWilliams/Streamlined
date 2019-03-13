78b1a68c9f47abebcd9da7b327d9f4318e6b1436
Seth Williams <swilliams9334@gmail.com>
2019-03-13 16:57:33 -0400

    getting the folders setup. the title changes like it should, but the image is staying at 'null' currently.

--- frontend/static/streamlined_project/src/App.js
+++ frontend/static/streamlined_project/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
         e.preventDefault();
     }
 
+
     render() {
         return(
             <div className='App'>
--- frontend/static/streamlined_project/src/modal.js
+++ frontend/static/streamlined_project/src/modal.js
@@ -6,49 +6,82 @@ import Card from 'react-bootstrap/Card';
 import action from './action.jpg'
 
 
-
-
 export default class FolderModal extends Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleInput = this.handleInput.bind(this);
 
-    this.state = {
-        show: false,
+        this.state = {
+            show: false,
+            folder_title: '',
+            icon: null,
+
+        };
+    }
+
+    handleInput(e) {
+        let obj = {};
+
+        if (e.target.name !== 'image') {
+            obj[e.target.name] = e.target.value;
+        } else {
+            let icon = e.target.files[0];
+            obj.file = icon;
+
+            let fileReader = new FileReader();
+            fileReader.onload = () => this.setState({icon: fileReader.result});
+            fileReader.readAsDataURL(icon);
+        }
+        this.setState(obj);
+        console.log(this.state.icon);
+        console.log(this.state.folder_title);
     };
-  }
 
-  handleClose() {
-    this.setState({ show: false});
-  }
+    handleClose() {
+        this.setState({show: false});
+    }
 
-  handleShow() {
-    this.setState({ show: true});
-  }
+    handleShow() {
+        this.setState({show: true});
+    }
 
-  render() {
-    return(
-      <div>
-        <Button variant="primary" onClick={this.handleShow}>
-            Add
-        </Button>
+    render() {
+        return (
+            <div>
+                <Button variant="primary" onClick={this.handleShow}>
+                    Add
+                </Button>
 
-        <Modal show={this.state.show} onClick={this.props.addFolder} onHide={this.handleClose}>
-            <Modal.Title>New Folder</Modal.Title>
-          <Modal.Body>Title:<input placeholder='genre, user, etc...' type="text"/></Modal.Body>
-            <Modal.Body><input type="file"/></Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={this.handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
-  }
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Title>New Folder</Modal.Title>
+                    <Modal.Body htmlFor='folder_title'>Title:
+                        <input
+                            name='folder_title'
+                            id='folder_title'
+                            onChange={this.handleInput}
+                            placeholder='genre, user, etc...'
+                            type="text"/>
+                    </Modal.Body>
+                    <Modal.Body htmlFor='icon'>Folder Art:
+                        <input
+                            name='icon'
+                            id='icon'
+                            onChange={this.handleInput}
+                            type="file"/>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
 }
