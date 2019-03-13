d5d5e34a737c38b88580d31ac81507e580b1d592
Seth Williams <swilliams9334@gmail.com>
2019-03-12 20:54:42 -0400

    added staticfiles

---
+++ staticfiles/streamlined_project/build/precache-manifest.0bbb56217c0956e0843732a4b656820f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "40d70fe1b09768276101",
+    "url": "/static/js/main.c2a1a5bb.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "40d70fe1b09768276101",
+    "url": "/static/css/main.347a5cbf.chunk.css"
+  },
+  {
+    "revision": "0c3ae96c072219883885daf024e42fb5",
+    "url": "/index.html"
+  }
+];
